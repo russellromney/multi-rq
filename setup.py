1c4b855f6988233cd6ee8defea84190fe68e8191
russellromney <russdude7@gmail.com>
2019-08-06 15:29:55 -0500

    release 0.2

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'multi-rq',
-    version = '0.1',
+    version = '0.2',
     description = 'Simple async multiprocessing with RQ',
     long_description = 'an extension of rq that emulates the mp.Pool.apply_async behavior in multiprocessing with a task queue',
-    keywords = ' dash rq redis plotly multiprocessing task queue',
+    keywords = ' dash rq redis plotly multiprocessing task queue parallel',
     url = 'https://github.com/russellromney/multi-rq',
     author = 'Russell Romney',
     author_email = 'russellromney@gmail.com',
